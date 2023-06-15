4d7a67e7aa98ff9b590324a2aeab1f8f1c1497bc
Von Aralar <von.aralar@gmail.com>
2023-06-15 16:48:18 +0800

    Life is not Daijobu

--- #1/index.ts
+++ #1/index.ts
@@ -1,10 +1,29 @@
 export default class<TInput extends Array<any> = Array<any>, TOutput = any> {
-  constructor(private handler: (...args: TInput) => Promise<TOutput>, private timeout?: number) {
+  private cache: Map<string, Promise<TOutput>> = new Map();
+
+  constructor(
+    private handler: (...args: TInput) => Promise<TOutput>,
+    private timeout?: number
+  ) {
     // do something
   }
 
   async exec(...args: TInput): Promise<TOutput> {
-    // do something
-    return {} as TOutput;
+    const key = JSON.stringify(args);
+
+    if (this.cache.has(key)) {
+      return this.cache.get(key)!;
+    }
+
+    const promise = this.handler(...args);
+    this.cache.set(key, promise);
+
+    if (this.timeout) {
+      setTimeout(() => {
+        this.cache.delete(key);
+      }, this.timeout);
+    }
+
+    return promise;
   }
 }
--- #2/index.ts
+++ #2/index.ts
@@ -1,22 +1,89 @@
+import { DateTime } from "luxon";
+import R from "ramda";
+import fs from "fs";
 /**
  * Calculates the ratio between the amount of time when status is AVAILABLE and
  * the amount of time between startDateTime inclusive and endDateTime exclusive.
- * @param startDateTime 
- * @param endDateTime 
+ * @param startDateTime
+ * @param endDateTime
  */
 export function availability(startDateTime: Date, endDateTime: Date): number {
-  // do something
-  return 0;
-}
+  const data = require("./data.json");
 
+  const startTime = startDateTime.getTime();
+  const endTime = endDateTime.getTime();
+  let availableDuration = 0;
+
+  for (const entry of data) {
+    const entryTime = new Date(entry.timestamp).getTime();
+
+    if (entryTime >= startTime && entryTime < endTime) {
+      if (entry.status === "AVAILABLE") {
+        availableDuration++;
+      }
+    }
+  }
+
+  const totalDuration = (endTime - startTime) / (1000 * 60);
+
+  return availableDuration / totalDuration;
+}
 /**
  * Generates the outages between startDateTime inclusive and endDateTime exclusive.
  * An outage is PARTIAL if the status within the period is PARTIALLY_AVAILABLE.
  * Similarly, an outage is MAJOR if the status within the period is UNAVAILABLE.
- * @param startDateTime 
- * @param endDateTime 
+ * @param startDateTime
+ * @param endDateTime
  */
-export function outages(startDateTime: Date, endDateTime: Date): { type: 'PARTIAL' | 'MAJOR', timestamp: Date, duration: number }[] {
-  // do something
-  return [];
+export function outages(
+  startDateTime: Date,
+  endDateTime: Date
+): { type: "PARTIAL" | "MAJOR"; timestamp: Date; duration: number }[] {
+  const data = require("./data.json");
+  const outages: {
+    type: "PARTIAL" | "MAJOR";
+    timestamp: Date;
+    duration: number;
+  }[] = [];
+
+  let outageStart: Date | null = null;
+  let outageStatus: "PARTIALLY_AVAILABLE" | "UNAVAILABLE" | null = null;
+
+  for (const item of data) {
+    const timestamp = new Date(item.timestamp);
+    if (timestamp >= startDateTime && timestamp < endDateTime) {
+      if (outageStatus !== item.status) {
+        if (outageStart && outageStatus) {
+          const outageEnd = timestamp;
+          const duration = Math.round(
+            (outageEnd.getTime() - outageStart.getTime()) / 60000 // Convert milliseconds to minutes
+          );
+
+          outages.push({
+            type: outageStatus === "PARTIALLY_AVAILABLE" ? "PARTIAL" : "MAJOR",
+            timestamp: outageStart,
+            duration: duration,
+          });
+        }
+
+        outageStart = timestamp;
+        outageStatus = item.status === "AVAILABLE" ? null : item.status;
+      }
+    }
+  }
+
+  if (outageStart && outageStatus) {
+    const outageEnd = endDateTime;
+    const duration = Math.round(
+      (outageEnd.getTime() - outageStart.getTime()) / 60000 // Convert milliseconds to minutes
+    );
+
+    outages.push({
+      type: outageStatus === "PARTIALLY_AVAILABLE" ? "PARTIAL" : "MAJOR",
+      timestamp: outageStart,
+      duration: duration,
+    });
+  }
+
+  return outages;
 }
